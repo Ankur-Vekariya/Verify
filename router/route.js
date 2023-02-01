385592e8fb768056c83a195dc8cb1f95e599efe9
ankur12v <65348445+ankur12v@users.noreply.github.com>
2023-02-01 23:07:04 +0530

    Fix bugs

--- router/route.js
+++ router/route.js
@@ -13,7 +13,9 @@ export default router;
 
 router.route("/register").post(controller.register);
 router.route("/registerMail").post(registerMail);
-router.route("/authenticate").post((req, res) => res.end());
+router
+  .route("/authenticate")
+  .post(controller.verifyUser, (req, res) => res.end());
 router.route("/login").post(controller.verifyUser, controller.login);
 
 // GET methods
@@ -22,7 +24,7 @@ router.route("/user/:username").get(controller.getUser);
 router
   .route("/generateOTP")
   .get(controller.verifyUser, localVariable, controller.generateOTP);
-router.route("/verifyOTP").get(controller.verifyOTP);
+router.route("/verifyOTP").get(controller.verifyUser, controller.verifyOTP);
 router.route("/createResetSession").get(controller.createResetSession);
 
 // PUT methods
